3ec85c49eb320847dd1dc7165e120bfe7505f08e
srinath15042007 <srinathseetha2007@gmail.com>
2026-06-19 12:08:44 -0400

    Add files via upload

---
+++ CodeAlpha_Detection_System/NIDS.py
@@ -0,0 +1,24 @@
+total 136
+drwx------  8 root root  4096 Jun 18 09:14 .
+drwxr-xr-x 18 root root  4096 Apr 23  2025 ..
+-rw-r--r--  1 root root  5551 Apr 23  2025 .bashrc
+-rw-r--r--  1 root root   607 Apr 23  2025 .bashrc.original
+-rw-rw-r--  1 root root   951 Jun 16 05:35 BNS.py
+drwx------  7 root root  4096 Jun 16 05:03 .cache
+drwx------  4 root root  4096 Jun 16 05:03 .config
+drwx------  3 root root  4096 Jun 16 05:03 .dbus
+-rw-r--r--  1 root root 11656 Apr 23  2025 .face
+lrwxrwxrwx  1 root root    11 Apr 23  2025 .face.icon -> /root/.face
+dr-x------  2 root root     0 Jun 16 05:03 .gvfs
+drwxrwxr-x  3 root root  4096 Jun 16 05:03 .local
+-rw-rw-r--  1 root root     0 Jun 18 09:15 NIDS.py
+-rw-rw-r--  1 root root  1235 Jun 18 09:14 NIDS.txt
+-rw-rw-r--  1 root root 40849 Jun 16 05:36 packets.log
+-rw-rw-r--  1 root root  1101 Jun 16 10:07 PAT.py
+-rw-r--r--  1 root root   132 Feb 16  2025 .profile
+-rw-rw-r--  1 root root   951 Jun 16 05:20 sri15.py
+-rw-rw-r--  1 root root   951 Jun 16 05:03 sri2.py
+drwx------  2 root root  4096 Apr 23  2025 .ssh
+-rw-r-----  1 root root     4 Jun 16 04:39 .vboxclient-display-svga-x11-tty1-control.pid
+-rw-------  1 root root   367 Jun 18 04:02 .zsh_history
+-rw-r--r--  1 root root 10868 Apr 23  2025 .zshrc
